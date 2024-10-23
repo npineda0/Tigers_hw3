38bec2612331e11af97bb369e1afddf7702153f2
npineda0 <97705502+npineda0@users.noreply.github.com>
2024-10-22 22:16:54 -0400

    contact page edited & styled

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -4,7 +4,15 @@ export default function Contact() {
 
     return (
         <main>
-            <h1>Contact</h1>
+            <div className={contactStyles.formBackground}>
+                <p className={contactStyles.contactHeader}>Contact Us!</p>
+                <form>
+                    <input className={contactStyles.contactInput} type="text" name="fullName" placeholder="Name"/> <br/>
+                    <input className={contactStyles.contactInput} type="text" name="email" placeholder="Email"/> <br/>
+                    <input className={contactStyles.contactInput} type="text" name="message" placeholder="Message"/> <br/>
+                    <button className={contactStyles.contactBtn} >Submit</button>
+                </form>
+            </div>
         </main>
     );
 }
