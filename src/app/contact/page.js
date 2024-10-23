34c5d8cf5c69c545007acd4826ee603813e6bf24
npineda0 <97705502+npineda0@users.noreply.github.com>
2024-10-22 22:30:47 -0400

    component added to contact page

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -1,17 +1,21 @@
 import contactStyles from "./contact.module.css";
+import TigerImage from "@/components/TigerImage/tiger";
 
 export default function Contact() {
 
     return (
         <main>
-            <div className={contactStyles.formBackground}>
-                <p className={contactStyles.contactHeader}>Contact Us!</p>
-                <form>
-                    <input className={contactStyles.contactInput} type="text" name="fullName" placeholder="Name"/> <br/>
-                    <input className={contactStyles.contactInput} type="text" name="email" placeholder="Email"/> <br/>
-                    <input className={contactStyles.contactInput} type="text" name="message" placeholder="Message"/> <br/>
-                    <button className={contactStyles.contactBtn} >Submit</button>
-                </form>
+            <div className={contactStyles.contactContainer}>
+                <div className={contactStyles.tigerImg}><TigerImage/></div>
+                <div className={contactStyles.formBackground}>
+                    <p className={contactStyles.contactHeader}>Contact Us!</p>
+                    <form>
+                        <input className={contactStyles.contactInput} type="text" name="fullName" placeholder="Name"/> <br/>
+                        <input className={contactStyles.contactInput} type="text" name="email" placeholder="Email"/> <br/>
+                        <input className={contactStyles.contactInput} type="text" name="message" placeholder="Message"/> <br/>
+                        <button className={contactStyles.contactBtn} >Submit</button>
+                    </form>
+                </div>
             </div>
         </main>
     );
